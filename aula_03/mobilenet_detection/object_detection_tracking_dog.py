2ed60c4f5a0314b3f0f98232765a27838e0bb281
thiagoverardo <38051267+thiagoverardo@users.noreply.github.com>
2019-03-13 09:51:17 -0300

    AteEx3

---
+++ aula_03/mobilenet_detection/object_detection_tracking_dog.py
@@ -0,0 +1,208 @@
+#Eu escolhi Cachorro
+# Para RODAR
+# python3 object_detection_tracking_dog.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+# Credits: https://www.pyimagesearch.com/2017/09/11/object-detection-with-deep-learning-and-opencv/
+
+print("Para executar:\npython object_detection_webcam.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel")
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+    help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+    help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+    help="minimum probability to filter weak detections")
+ap.add_argument("-v", "--video", type=str,
+    help="path to input video file")
+ap.add_argument("-t", "--tracker", type=str, default="kcf",
+    help="OpenCV object tracker type")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+    "sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+(major, minor) = cv2.__version__.split(".")[:2]
+
+# if we are using OpenCV 3.2 OR BEFORE, we can use a special factory
+# function to create our object tracker
+if int(major) == 3 and int(minor) < 3:
+    tracker = cv2.Tracker_create(args["tracker"].upper())
+
+# otherwise, for OpenCV 3.3 OR NEWER, we need to explicity call the
+# approrpiate object tracker constructor:
+else:
+    # initialize a dictionary that maps strings to their corresponding
+    # OpenCV object tracker implementations
+    OPENCV_OBJECT_TRACKERS = {
+        "csrt": cv2.TrackerCSRT_create,
+        "kcf": cv2.TrackerKCF_create,
+        "boosting": cv2.TrackerBoosting_create,
+        "mil": cv2.TrackerMIL_create,
+        "tld": cv2.TrackerTLD_create,
+        "medianflow": cv2.TrackerMedianFlow_create,
+        "mosse": cv2.TrackerMOSSE_create
+    }
+
+    # grab the appropriate object tracker using our dictionary of
+    # OpenCV object tracker objects
+    tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+
+
+# load the input image and construct an input blob for the image
+# by resizing to a fixed 300x300 pixels and then normalizing it
+# (note: normalization is done via the authors of the MobileNet SSD
+# implementation)
+
+
+def detect(frame):
+    image = frame.copy()
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    print("[INFO] computing object detections...")
+    net.setInput(blob)
+    detections = net.forward()
+
+    results = []
+
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+
+
+        if confidence > args["confidence"]:
+            # extract the index of the class label from the `detections`,
+            # then compute the (x, y)-coordinates of the bounding box for
+            # the object
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            if CLASSES[idx] == "dog":
+                # display the prediction
+                label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+                print("[INFO] {}".format(label))
+                cv2.rectangle(image, (startX, startY), (endX, endY),
+                    COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(image, label, (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+                results.append((CLASSES[idx], confidence*100, (startX, startY),(endX, endY) ))
+            
+    # show the output image
+    return image, results
+
+
+initBB = None
+fps = None
+
+
+import cv2
+
+#cap = cv2.VideoCapture('hall_box_battery_1024.mp4')
+cap = cv2.VideoCapture(0)
+
+contador = 0
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    (H, W) = frame.shape[:2]
+
+    #se o contador for menor que 5, detectar.
+    if contador < 5:
+        result_frame, result_tuples = detect(frame)
+
+        for a in result_tuples:
+            if a[0] == "dog":
+                contador +=1
+            else:
+                contador = 0
+
+        if len(result_tuples) == 0:
+            contador = 0
+
+        cv2.imshow('Frame',result_frame)
+
+    if contador >= 5:
+
+        if initBB is not None:
+            # grab the new bounding box coordinates of the object
+            (success, box) = tracker.update(frame)
+
+            # check to see if the tracking was a success
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                cv2.rectangle(frame, (x, y), (x + w, y + h),
+                    (0, 255, 0), 2)
+            else:
+                contador = 0
+
+            # update the FPS counter
+            fps.update()
+            fps.stop()
+
+            info = [
+                ("Tracker", args["tracker"]),
+                ("Success", "Yes" if success else "No"),
+                ("FPS", "{:.2f}".format(fps.fps())),
+            ]
+
+            # loop over the info tuples and draw them on our frame
+            for (i, (k, v)) in enumerate(info):
+                text = "{}: {}".format(k, v)
+                cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+        # Display the resulting frame
+    cv2.imshow('Frame',frame)
+
+    if contador >= 5:
+            # ("CLASS", confidence, (x1, y1, x2, y3))
+        x_1, y_1 = result_tuples[0][2]
+        x_2, y_2 = result_tuples[0][3]
+        height = y_2 - y_1
+        width = x_2-x_1
+
+        # select the bounding box of the object we want to track (make
+        # sure you press ENTER or SPACE after selecting the ROI)
+        initBB = (x_1, y_1, abs(width),abs(height))
+        # start OpenCV object tracker using the supplied bounding box
+        # coordinates, then start the FPS throughput estimator as well
+        tracker.init(frame, initBB)
+        fps = FPS().start()
+
+    # if the `q` key was pressed, break from the loop
+    elif cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
